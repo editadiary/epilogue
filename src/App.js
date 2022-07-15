81f160b65e4e9132e00c751d7f59850a8dcecc08
editadiary <blessedita@kaist.ac.kr>
2022-07-15 17:03:03 +0900

    home-page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' exact />
+          <Route exact path='/' element={<Home/>} />
+          {/* <Route path='/services' element={<Services/>} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/sign-up' element={<SignUp/>} /> */}
         </Routes>
-    </Router>
+      </Router>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './Button';
 import './Navbar.css'
@@ -15,17 +15,20 @@ function Navbar() {
     if(window.innerWidth <= 960) {
       setButton(false)
     }else{
-      setButton(true)
+      setButton(true )
     }
   };
-
+ 
+  useEffect(() => {
+    showButton();
+  }, []);
   window.addEventListener('resize', showButton); //whenever resize the screen
 
   return (
     <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <Link to="/" className='navbar-logo'>
+                <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
                     EPILOGUE <i className='fab fa-typo3'/>
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
