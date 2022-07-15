848df37f99ecfdd59a632d7822560580561042b7
editadiary <blessedita@kaist.ac.kr>
2022-07-15 21:02:54 +0900

    main-page-done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Footer from './components/Footer';
 function App() {
   return (
     <>
@@ -11,10 +14,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path='/' element={<Home/>} />
-          {/* <Route path='/services' element={<Services/>} />
+          <Route path='/services' element={<Services/>} />
           <Route path='/products' element={<Products/>} />
-          <Route path='/sign-up' element={<SignUp/>} /> */}
+          <Route path='/sign-up' element={<SignUp/>} />
         </Routes>
+        <Footer/>
       </Router>
     </>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,6 +3,7 @@ import '../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
 
+
 function Home() {
   return (
     <>
